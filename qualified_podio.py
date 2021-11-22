01758a8fee6f959bea6e60d4458962523b2eeada
julianomata <jfmatta@gmail.com>
2021-11-22 12:19:01 -0300

    Testando alteração nome da variável.

--- qualified_podio.py
+++ qualified_podio.py
@@ -1,9 +1,9 @@
 def podio_olimpico(tempo_chegada1, tempo_chegada2, tempo_chegada3):
     lista1 = [tempo_chegada1, tempo_chegada2, tempo_chegada3]
-    ordenado = sorted(lista1)
+    podio_ordenado = sorted(lista1)
     podio = (
-            f"1 - {ordenado[0]} minutos\n"
-            f"2 - {ordenado[1]} minutos\n"
-            f"3 - {ordenado[2]} minutos\n"
+            f"1 - {podio_ordenado[0]} minutos\n"
+            f"2 - {podio_ordenado[1]} minutos\n"
+            f"3 - {podio_ordenado[2]} minutos\n"
         )
     return podio
